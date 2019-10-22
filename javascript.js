770f0a0d875a39a54e9dfd4da73a1ce30c697727
agospadaro <you@example.com>
2019-10-21 22:14:45 -0300

    agregué ejercicios 34, 36 y 37 y corregí sugerencias

--- javascript.js
+++ javascript.js
@@ -23,7 +23,7 @@ console.log("El resto de 3 / 2 es:", numero1 % numero2);
 
 let numero = 9;
 
-console.log("La tabla del 9:" + " " + numero*1, numero*2, numero*3, numero*4, numero*5, numero*6, numero*7, numero*8, numero*9, numero*10);
+console.log("La tabla del 9:" + " " + numero * 1, numero * 2, numero * 3, numero * 4, numero * 5, numero * 6, numero * 7, numero * 8, numero * 9, numero * 10);
 
 
 
@@ -32,7 +32,7 @@ console.log("La tabla del 9:" + " " + numero*1, numero*2, numero*3, numero*4, nu
 
 let ladoCuadrado = 10;
 
-console.log("El perímetro del cuadrdo es:" + " " + ladoCuadrado*4);
+console.log("El perímetro del cuadrdo es:" + " " + ladoCuadrado * 4);
 
 
 
@@ -41,7 +41,7 @@ console.log("El perímetro del cuadrdo es:" + " " + ladoCuadrado*4);
 
 let lado = 5;
 
-console.log("El área de un cuadrado es:" + " " + lado*2);
+console.log("El área de un cuadrado es:" + " " + lado * 2);
 
 
 
@@ -88,13 +88,55 @@ console.log("Cantidad de personas:" + " " + cantidadDePersonas);
 
 
 
+/********* EJERCICIO 34 *********/
+
+
+let ingresos = 1000;
+
+let ganancias = 600;
+
+let impuestos = 500;
+
+console.log("Objetivo de ingresos: true o false", ingresos >= 800);
+
+console.log("Objetivo de impuestos:", impuestos <= 400);
+
+console.log("Bono de ganancias:", ganancias == 600);
+
+
+
 /********* EJERCICIO 35 *********/
 
 
-let numeroDeCliente = 143245
+let numeroDeCliente = 143245;
 
-let numeroDeClienteIngresado = "143245"
+let numeroDeClienteIngresado = "143245";
 
-console.log("El número ingresado por el cliente es correcto:", numeroDeCliente == numeroDeClienteIngresado)
+console.log("El número ingresado por el cliente es correcto:", numeroDeCliente == numeroDeClienteIngresado);
 
-console.log("El cliente ingresó un número de cliente válido:", numeroDeCliente === numeroDeClienteIngresado)
+console.log("El cliente ingresó un número de cliente válido:", numeroDeCliente === numeroDeClienteIngresado);
+
+
+
+/********* EJERCICIO 36 *********/
+
+let numeroUno = 10;
+
+let numeroDos = 20;
+
+console.log("Las variables numero1 y numero2 tienen el mismo valor:", numero1 != numero2);
+
+console.log("Las variables numero1 y numero2 son distintas:", numero1 !== numero2);
+
+
+
+/********* EJERCICIO 37 *********/
+
+
+let promedioParaAprobar = 6;
+
+let promedioDelAalumno = 4;
+
+console.log("El alumno tiene un promedio mayor o igual al requerido:", promedioDelAalumno >= promedioParaAprobar);
+
+console.log("Al alumno le faltaron", (promedioParaAprobar - promedioDelAalumno), "para aprobar");
